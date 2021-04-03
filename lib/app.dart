04e549b1727264d0933405f5cae9a23f93d0e58f
yafetszt <42559608+yafetszt@users.noreply.github.com>
2021-04-03 17:17:41 -0500

    Routes for navigation between screens.

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/ui/screens/login.dart';
+
+class RecipeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Recipes',
+      initialRoute: '/login',
+      routes: {
+        '/': (context) => LoginScreen(),
+        '/login': (context) => LoginScreen(),
+      }
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/app.dart';
+
+void main() => runApp(
+  new RecipeApp(),
+);
